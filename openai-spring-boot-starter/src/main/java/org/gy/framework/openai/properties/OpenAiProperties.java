0a7ef7772291648e05c1097d169f0580b083a06d
guanyang <guanyangsunlight@163.com>
2023-03-05 20:48:28 +0800

    update openai config

--- openai-spring-boot-starter/src/main/java/org/gy/framework/openai/properties/OpenAiProperties.java
+++ openai-spring-boot-starter/src/main/java/org/gy/framework/openai/properties/OpenAiProperties.java
@@ -1,13 +1,18 @@
 package org.gy.framework.openai.properties;
 
+import java.util.Objects;
+import java.util.Optional;
 import lombok.Data;
 import org.gy.framework.openai.utils.OpenAiUtils;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.util.StringUtils;
 
 @Data
 @ConfigurationProperties(prefix = "openai")
 public class OpenAiProperties implements InitializingBean {
+
+    public static final String TOKEN_KEY = "OPENAI_TOKEN";
 
     // 秘钥
     String token;
@@ -17,7 +22,13 @@ public class OpenAiProperties implements InitializingBean {
     // 设置属性时同时设置给OpenAiUtils
     @Override
     public void afterPropertiesSet() throws Exception {
-        OpenAiUtils.OPENAPI_TOKEN = token;
+        token = Optional.ofNullable(token).filter(StringUtils::hasText).orElseGet(() -> getProperty(TOKEN_KEY));
+        OpenAiUtils.OPENAPI_TOKEN = Objects.requireNonNull(token, () -> "openai token is null");
         OpenAiUtils.TIMEOUT = timeout;
+    }
+
+    private static String getProperty(String key) {
+        String value = System.getProperty(key);
+        return StringUtils.hasText(value) ? value : System.getenv(key);
     }
 }
